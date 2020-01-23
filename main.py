fee6b99dd5ec1004c08af260914c25e8e613ceec
pol <121955907@qq.com>
2020-01-23 18:59:28 +0800

    feat: env config

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
 # coding=utf-8
 
+import sys
 from flask import Flask
 from App.restful_env import restful_build
+from gevent.pywsgi import WSGIServer
 
 app = Flask(__name__)
 restful_build(app)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=9990)  # 开启
+    if len(sys.argv) > 1 and sys.argv[1] == 'prod':
+        print('prod Env')
+        http_server = WSGIServer(('', 9990), app)
+        http_server.serve_forever()
+    else:
+        print('dev Env')
+        app.run(debug=True, port=9990)  # 开启
