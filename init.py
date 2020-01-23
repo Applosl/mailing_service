a789e0c24ef3f783b27565610047d1d304eed6d2
pol <121955907@qq.com>
2020-01-23 18:38:38 +0800

    feat: init.py upgrade

--- init.py
+++ init.py
@@ -13,11 +13,11 @@ if __name__ == "__main__":
 
     with open('config/DB.py', 'w') as f:
         db_config = [
-            'class DbConfig:',
-            '\tSQLALCHEMY_DATABASE_URI = ""  # MYSQL CONFIG',
-            '\tSQLALCHEMY_ECHO = False  # 是否输出SQL在控制台 用于调试',
-            '\tSQLALCHEMY_COMMIT_ON_TEARDOWN = True  # 在2.0下该配置被去除',
-            '\tSQLALCHEMY_TRACK_MODIFICATIONS = False',
+            'class DbConfig:\n',
+            '\tSQLALCHEMY_DATABASE_URI = ""  # MYSQL CONFIG\n',
+            '\tSQLALCHEMY_ECHO = False  # 是否输出SQL在控制台 用于调试\n',
+            '\tSQLALCHEMY_COMMIT_ON_TEARDOWN = True  # 在2.0下该配置被去除\n',
+            '\tSQLALCHEMY_TRACK_MODIFICATIONS = False\n',
         ]
         f.writelines(db_config)
 
@@ -27,12 +27,12 @@ if __name__ == "__main__":
 
     with open('config/Mail.py', 'w') as f:
         mail_config = [
-            'class MailConfig:',
-            '\tMAIL_SERVER = "smtp.qq.com"  # 邮件服务器地址',
-            '\tMAIL_PORT = "587"  # 端口',
-            '\tMAIL_USE_TLS = True  # 是否使用TLS',
-            '\tMAIL_USERNAME = ""  # 用户名',
-            '\tMAIL_PASSWORD = ""  # 密码',
-            '\tMAIL_DEFAULT_SENDER = ""  # 默认发件人名称',
+            'class MailConfig:\n',
+            '\tMAIL_SERVER = "smtp.qq.com"  # 邮件服务器地址\n',
+            '\tMAIL_PORT = "587"  # 端口\n',
+            '\tMAIL_USE_TLS = True  # 是否使用TLS\n',
+            '\tMAIL_USERNAME = ""  # 用户名\n',
+            '\tMAIL_PASSWORD = ""  # 密码\n',
+            '\tMAIL_DEFAULT_SENDER = ""  # 默认发件人名称\n',
         ]
         f.writelines(mail_config)
